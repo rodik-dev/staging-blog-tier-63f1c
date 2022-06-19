4abdfbfc89a3f47854df1d1105bc8e791f084a79
rodik-dev <42075304+rodik-dev@users.noreply.github.com>
2022-06-19 15:41:43 +0300

    fsdf

--- src/components/atoms/Action/index.tsx
+++ src/components/atoms/Action/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import Link from '../Link';
 import { iconMap } from '../../svgs';
-
+// x
 export default function Action(props) {
     const {
         type,
